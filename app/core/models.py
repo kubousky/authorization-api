e5c0f5c8737c651b8864b0ec3d7579edf4133181
Kuba <kparcheta@gmail.com>
2022-10-28 19:54:28 +0200

    User

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,40 @@
-from django.db import models  # noqa
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    """Manager for user."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a ne user."""
+        if not email:
+            raise ValueError('Users must have email address')
+        # self.model refers to a User
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        # using=self._db - just in case of adding multiple databases
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
